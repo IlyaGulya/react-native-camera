59c3107228bb11b6764b18c4b06a7dcf88a6073b
sysoev.m <sysoev.m@devicement.com>
2018-02-10 13:05:46 +0700

    Merge branch 'barcode-finder' of https://github.com/fbacker/react-native-camera into qr_finder_integration
    # Conflicts:
    #	android/src/main/java/com/lwansbrough/RCTCamera/MutableImage.java
    #	android/src/main/java/com/lwansbrough/RCTCamera/RCTCameraModule.java
    #	android/src/main/java/com/lwansbrough/RCTCamera/RCTCameraViewFinder.java
    #	ios/RCT/RCTCameraManager.h
    #	ios/RCT/RCTCameraManager.m
    #	src/index.js

--- src/Camera.js
+++ src/Camera.js
@@ -8,12 +8,13 @@ import {
   StyleSheet,
   requireNativeComponent,
   ViewPropTypes,
-  PermissionsAndroid,
-  ActivityIndicator,
+  Dimensions,
   View,
   Text,
 } from 'react-native';
+import BarcodeFinder from './BarcodeFinder';
 
+const {height, width} = Dimensions.get('window');
 const CameraManager = NativeModules.CameraManager || NativeModules.CameraModule;
 const CAMERA_REF = 'camera';
 
@@ -101,6 +102,11 @@ export default class Camera extends Component {
     permissionDialogMessage: PropTypes.string,
     notAuthorizedView: PropTypes.element,
     pendingAuthorizationView: PropTypes.element,
+    barcodeFinderVisible: PropTypes.bool,
+    barcodeFinderWidth: PropTypes.number,
+    barcodeFinderHeight: PropTypes.number,
+    barcodeFinderStyle: PropTypes.object,
+    barcodeFinderPercentageSize: PropTypes.array,
   };
 
   static defaultProps = {
@@ -120,6 +126,11 @@ export default class Camera extends Component {
     cropToPreview: false,
     clearWindowBackground: false,
     barCodeTypes: Object.values(CameraManager.BarCodeType),
+    barcodeFinderVisible: false,
+    barcodeFinderWidth: 200,
+    barcodeFinderHeight: 200,
+    barcodeFinderStyle: {borderColor: "rgba(255,255,255,0.6)", borderWidth: 1},
+    barcodeFinderComponent: <BarcodeFinder />,
     permissionDialogTitle: '',
     permissionDialogMessage: '',
     notAuthorizedView: (
@@ -240,14 +251,36 @@ export default class Camera extends Component {
     }
   }
 
+  _child(){
+    var props = {
+      style: this.props.barcodeFinderStyle,
+      width: this.props.barcodeFinderWidth,
+      height: this.props.barcodeFinderHeight
+    }
+    return React.cloneElement(this.props.barcodeFinderComponent, props);
+  }
+
   render() {
+    // Should we show barcode finder, use in child or use default
+    var childs = null;
+    var barcodeFinderPercentageSize = [0,0];
+    if(this.props.barcodeFinderVisible){
+      // we need % size of viewFinder
+      barcodeFinderPercentageSize = [(this.props.barcodeFinderWidth/width),(this.props.barcodeFinderHeight/height)]
+      childs = <View style={{left:0,right:0,bottom:0,top:0,position:'absolute',justifyContent:'center',alignItems:'center'}}><View style={{width:this.props.barcodeFinderWidth,height:this.props.barcodeFinderHeight}}>{this._child()}</View></View>;
+    }
     // TODO - style is not used, figure it out why
     // eslint-disable-next-line
     const style = [styles.base, this.props.style];
-    const nativeProps = convertNativeProps(this.props);
+    const nativeProps = convertNativeProps(Object.assign({},this.props,{barcodeFinderPercentageSize}));
 
     if (this.state.isAuthorized) {
-      return <RCTCamera ref={CAMERA_REF} {...nativeProps} />;
+      return <View style={{flex:1}}>
+              <View style={{flex:1}}>
+                <RCTCamera ref={CAMERA_REF} {...nativeProps} />
+              </View>
+              {childs}
+            </View>);
     } else if (!this.state.isAuthorizationChecked) {
       return this.props.pendingAuthorizationView;
     } else {
